58637d3a652e3c0fe803c02f7d43c2fa04bc9381
1000098423 <rupanjalisaha2001@gmail.com>
2026-04-23 18:31:13 +0530

    changes committed

--- src/main/java/com/projects/sms/controller/PostController.java
+++ src/main/java/com/projects/sms/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -73,9 +74,9 @@ public class PostController {
 	
     @PostMapping("/blogsDetails/{postId}")
     public ResponseEntity<?> toggleLike(@PathVariable Long postId) {
-    	String post_username = postRepository.findUsernameBypostId(postId);
-    	Blogger blogger = userRepository.findByUsername(post_username).orElseThrow();
-    	Long userId = blogger.getId();
+    	Long userId = ((Blogger) SecurityContextHolder.getContext()
+	            .getAuthentication())
+	            .getId();
         return ResponseEntity.ok(likeService.toggleLike(userId, postId));
     }
 
